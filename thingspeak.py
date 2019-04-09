1ecd01a0e6956853b8d23ad41e50a464805ecdb6
Ralf Vogler <ralf.vogler@gmail.com>
2019-04-10 00:58:46 +0200

    publish values over mqttt every 10s, and (still) every 60s to thingspeak

--- thingspeak.py
+++ thingspeak.py
@@ -8,17 +8,23 @@ from tsl2561 import TSL2561, TSL2561_GAIN_16X
 
 tsl = TSL2561(gain=TSL2561_GAIN_16X, autogain=True)
 
+import paho.mqtt.client as mqtt
+client = mqtt.Client()
+client.connect("localhost")
+client.loop_start()
+import json
+
 class Config:
-  interval = 1
+  interval = 10
   url = 'https://api.thingspeak.com/update'
   key = 'ABCDEFGH12345678'
 
 
-def sendData(url, key, temp, pres, humi, lux, bb, ir):
-  values = { 'api_key': key, 'field1': temp, 'field2': pres, 'field3': humi, 'field4': lux, 'field5': bb, 'field6': ir }
+def postThingspeak(temp, pres, humi, lux, bb, ir):
+  values = { 'api_key': Config.key, 'field1': temp, 'field2': pres, 'field3': humi, 'field4': lux, 'field5': bb, 'field6': ir }
 
   postdata = urllib.urlencode(values)
-  req = urllib2.Request(url, postdata)
+  req = urllib2.Request(Config.url, postdata)
 
   log = time.strftime("%d-%m-%Y,%H:%M:%S") + ","
   log += "{:.2f}C".format(temp) + ","
@@ -40,16 +46,31 @@ def sendData(url, key, temp, pres, humi, lux, bb, ir):
   except Exception, e:
     log += type(e).__name__ + ': ' + e.message
 
-  print log
+  print(log)
+  sys.stdout.flush()
 
 def main():
+  i = 1
   while True:
-    temperature,pressure,humidity = bme280.readBME280All()
+    temp,pres,humi = bme280.readBME280All()
+    r_bme280 = { 'temperature': temp, 'pressure': pres, 'humidity': humi }
+
     bb, ir = tsl._get_luminosity()
     lux = tsl._calculate_lux(bb, ir)
-    sendData(Config.url, Config.key, temperature, pressure, humidity, lux, bb, ir)
-    sys.stdout.flush()
-    time.sleep(Config.interval*60)
+    r_tsl2561 = { 'lux': lux, 'broadband': bb, 'infrared': ir }
+
+    # mqtt
+    client.publish("sensors/bme280", json.dumps(r_bme280))
+    client.publish("sensors/tsl2561", json.dumps(r_tsl2561))
+
+    if i == 6:
+        postThingspeak(temp, pres, humi, lux, bb, ir)
+        i = 1
+    else:
+        i += 1
+
+    time.sleep(Config.interval)
+
 
 if __name__=="__main__":
    main()
